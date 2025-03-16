f25b3145d692b1dc925e94c6fc1a8cf95631d072
RameshEdirisinghe <e2145434@bit.uom.lk>
2025-03-16 23:19:39 +0530

    create service class

---
+++ src/main/java/edu/icet/service/impl/ExamServiceImpl.java
@@ -0,0 +1,50 @@
+package edu.icet.service.impl;
+
+import edu.icet.dto.Exam;
+import edu.icet.entity.ExamEntity;
+import edu.icet.repository.ExamRepository;
+import edu.icet.service.ExamService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class ExamServiceImpl implements ExamService {
+    final ExamRepository repository;
+    final ModelMapper mapper;
+
+    @Override
+    public void createExam(Exam exam) {
+        repository.save(mapper.map(exam, ExamEntity.class));
+    }
+
+    @Override
+    public Exam getExamByCourseId(Integer id) {
+       return mapper.map(repository.getByCourseId(id),Exam.class);
+    }
+
+    @Override
+    public List<Exam> getAll() {
+        return null;
+    }
+
+    @Override
+    public void updateExam(Exam exam) {
+
+    }
+
+    @Override
+    public void deleteExamById(Integer id) {
+
+    }
+
+    @Override
+    public void deleteExamByCourseId(Integer integer) {
+
+    }
+}
